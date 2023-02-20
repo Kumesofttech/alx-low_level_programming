743902c9e2eced60b46dd6a6ea533456c38fd4a2
Kufre Inyang <kumen88@gmail.com>
2023-02-20 13:27:39 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the standard error
+ * Return 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art us useful" -Dora Korpand 2023-02-20\n", stdout);
+Return (1);
+}
