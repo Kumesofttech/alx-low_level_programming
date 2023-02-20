1b07140a63a9006ea414c22571bccbf7b4039632
Kufre Inyang <kumen88@gmail.com>
2023-02-20 06:17:10 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints "with proper grammer, but the outcome is a piece of art," followed newline
+ * main - prints "with proper grammer a piece of art," followed newline
  * Return: Always 0,
- *
- */
+  */
 
 int main(void)
 {
-printf("w0ith proper grammar, but the outcome is a piece of art,\n");
-return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
 }
