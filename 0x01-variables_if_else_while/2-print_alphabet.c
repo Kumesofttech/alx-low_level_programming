9896b71876e649e1cffa4258ef737d6b7c332b73
Kufre Inyang <kumen88@gmail.com>
2023-02-22 17:12:46 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Empty point
+ * Description: 'Print alphabets lowercase'
+ * Return: AAlways 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+
+}
